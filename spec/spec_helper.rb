be7040ce7ac62f6f4c66a30e92a9a3d90a70fb52
Nicola Bernardini <nicb@sme-ccppd.org>
2016-01-03 06:35:35 +0100

    further spec_helper.rb streamlining

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,13 @@
-#if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  SimpleCov.start do
-    add_filter '/spec/'
-    add_filter '/lib/tasks/'
-    add_filter '/lib/FFI/utilities/suffixes.rb' # partially obsolete
-    formatter SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      CodeClimate::TestReporter::Formatter
-    ]
-  end
-# else
-# 	require 'simplecov'
-#   SimpleCov.start do
-#     add_filter '/spec/'
-#     add_filter '/lib/tasks/'
-#     add_filter '/lib/FFI/utilities/suffixes.rb' # partially obsolete
-#   end
-# end
+require 'codeclimate-test-reporter'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/lib/tasks/'
+  add_filter '/lib/FFI/utilities/suffixes.rb' # partially obsolete
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ])
+end
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'FFI/utilities'
 require 'byebug'
